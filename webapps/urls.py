899c85f7ca8bd91779c123dcd1e27784dba09f2e
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-03-12 22:20:35 +0530

    Added admin functionality

--- webapps/urls.py
+++ webapps/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^monopoly/', include('monopoly.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
